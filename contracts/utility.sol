20b49ace643aa1c94812c45e261553a8d8633275
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-01-24 20:51:22 -0500

    [+~] Implemented ERC721
      - Inherits from ERC721 and ERC165
      - Application Core
      - mint is from T721, but was required for testing
    
    CTS #1

---
+++ contracts/utility.sol
@@ -0,0 +1,22 @@
+//
+//    /$$    /$$$$$$$$ /$$$$$$    /$$
+//   | $$   |_____ $$//$$__  $$ /$$$$
+//  /$$$$$$      /$$/|__/  \ $$|_  $$
+// |_  $$_/     /$$/   /$$$$$$/  | $$
+//   | $$      /$$/   /$$____/   | $$
+//   | $$ /$$ /$$/   | $$        | $$
+//   |  $$$$//$$/    | $$$$$$$$ /$$$$$$
+//    \___/ |__/     |________/|______/
+//  t721: 0.1.0, sol: 0.5.0
+
+pragma solidity 0.5.0;
+
+library utility {
+
+    function isContract(address addr) internal view returns (bool) {
+        uint256 size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+
+}
