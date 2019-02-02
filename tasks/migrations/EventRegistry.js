b86e263b8b62327ee3487eab6b981f4e36d13204
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-02-01 19:27:58 -0500

    [+~] Deployment with gulp
    CTS #1

--- tasks/migrations/EventRegistry.js
+++ tasks/migrations/EventRegistry.js
@@ -1,14 +1,15 @@
 const {exec} = require('child_process');
 const {Portalize} = require('portalize');
 
-const T721 = async () => {
+const EventRegistry = async () => {
     return new Promise((ok, ko) => {
         Portalize.get.setPortal('./portal');
         Portalize.get.setModuleName('contracts');
 
         const config = Portalize.get.get('network.json', {module: 'network'});
-        const er_artifact = require('../../build/contracts/EventRegistryV0.json');
-        exec(`./node_modules/.bin/zos create T721 --init initialize --args ${er_artifact.networks[config.network_id].address},ticket721,T721 --network ${process.env.T721_NETWORK}`,
+        const admin_artifact = require('../../build/contracts/AdministrationBoardV0.json');
+        const emr_artifact = require('../../build/contracts/EventManagersRegistryV0.json');
+        exec(`./node_modules/.bin/zos create EventRegistry --init initialize --args ${admin_artifact.networks[config.network_id].address},${emr_artifact.networks[config.network_id].address} --network ${process.env.T721_NETWORK}`,
             (err, stdout, stderr) => {
                 if (err) {
                     console.error(stderr);
@@ -21,4 +22,4 @@ const T721 = async () => {
     })
 };
 
-module.exports = T721;
+module.exports = EventRegistry;
--- tasks/migrations/index.js
+++ tasks/migrations/index.js
@@ -2,5 +2,7 @@ const {series} = require('gulp');
 
 const AdministrationBoard = require('./AdministrationBoard');
 const T721 = require('./T721');
+const EventManagersRegistry = require('./EventManagersRegistry');
+const EventRegistry = require('./EventRegistry');
 
-module.exports = series(AdministrationBoard, T721);
+module.exports = series(AdministrationBoard, EventManagersRegistry, EventRegistry, T721);
