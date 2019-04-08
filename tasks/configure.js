def92c4406db7a807817c89639fbcb720559f1f4
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-04-08 17:43:01 -0400

    feat: add events_utility
      - Empty template
      - Configure tasks fills return with checksum condition
      - Checks for code match onchain

--- tasks/configure.js
+++ tasks/configure.js
@@ -1,6 +1,7 @@
 const signale = require('signale');
 const {local_configure} = require('./network/local');
 const {test_configure} = require('./network/test');
+const event_checksum_contract = require('./event_checksum_contract');
 const combiner = require('../events');
 
 const configure = async () => {
@@ -10,15 +11,15 @@ const configure = async () => {
     switch (process.env.T721_NETWORK) {
         case 'local':
             await local_configure();
-            await combiner(version, '0.5.0');
             break;
         case 'test':
             await test_configure();
-            await combiner(version, '0.5.0');
             break;
         default:
             throw new Error(`Unknown Network ${process.env.T721_NETWORK}`)
     }
+    await combiner(version, '0.5.0');
+    await event_checksum_contract();
     signale.info('[contracts][configured]');
 };
 
