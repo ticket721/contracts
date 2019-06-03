bd400dc826dfc06f48bbdde625f327f28b9761ce
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-06-03 17:48:37 -0400

    fix: upload deployment height for main contracts

--- tasks/migrations/index.js
+++ tasks/migrations/index.js
@@ -1,21 +1,25 @@
 const {from_current} = require('../misc');
-const {exec} = require('child_process');
+const {spawn} = require('child_process');
 
 const migrations = async () => {
     const current_dir = process.cwd();
     process.chdir(from_current(''));
 
     return new Promise((ok, ko) => {
-        exec(`${from_current('./node_modules/.bin/truffle')} migrate --network ${process.env.T721_NETWORK}`,
-            (err, stdout, stderr) => {
-                console.log(stdout);
-                console.error(stderr);
-                if (err) {
-                    return ko(err);
-                }
-                process.chdir(current_dir);
-                ok();
-            });
+        const child = spawn(`${from_current('./node_modules/.bin/truffle')}`,  [`migrate`, `--network`, `${process.env.T721_NETWORK}`]);
+        child.stdout.on('data', (data) => {
+            process.stdout.write(data.toString());
+        });
+        child.stderr.on('data', (data) => {
+            process.stderr.write(data.toString());
+        });
+        child.on('exit', (code) => {
+            if (code !== 0) {
+                return ko();
+            }
+            process.chdir(current_dir);
+            ok();
+        })
     })
 };
 
