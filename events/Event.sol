ebf4174ea3234d3f41f4fb922e5012f42b20e9a7
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-07-09 14:41:40 +0200

    feat: support ether withdrawal
    resolves #18

--- events/Event.sol
+++ events/Event.sol
@@ -30,6 +30,12 @@ contract Event/*$${{NAME}}$$*/ is /*$${{INHERITANCE}}$$*/ {
         T721V0(t721).register(owner);
     }
 
+    function withdraw(uint amount) public {
+        require(msg.sender == owner, "Only owner can withdraw funds");
+        require(address(this).balance >= amount, "Insuficient funds");
+        msg.sender.transfer(amount);
+    }
+
     constructor(address _t721/*$${INITIALIZER_ARGS}$$*/) public {
         t721 = _t721;
         minter_set_T721(_t721);
